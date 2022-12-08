70db766ffa07b1bd6986a3a93fdae79f817ed6e9
Jef Peniche <jef.peniche@gmail.com>
2022-12-08 10:13:42 -0600

    fix cors and change api success message

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,12 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
-import cors from "express";
+import express from "express";
+import cors from "cors";
 import { userRoute } from "./user/infrastructure/route/user.route";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  next();
-});
 app.use(userRoute);
 
 const port = process.env.PORT || 3001;
