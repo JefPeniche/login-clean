49025ecf35f020b69bf062351e8732049f4d56b2
DaniPoot <danielpoot.uc2@gmail.com>
2022-12-08 07:12:06 -0600

    Create the mock file for the test

---
+++ test/user/mocks/user.mock.ts
@@ -0,0 +1,8 @@
+import { User } from '../../../src/user/domain/dtos/user.dto'
+
+export const userMock: User = {
+  id: '223',
+  name: 'John Doe',
+  email: 'johnDoe@test.com',
+  password: 'password'
+}
